2a57cd315f30291e8bf39bc0d988957bd36c4974
ingyu1008 <ingyu1008@gmail.com>
2021-04-28 14:38:52 +0900

    ADD : Strategy Pattern Practice

---
+++ Strategy Pattern/GameCharacter.java
@@ -0,0 +1,15 @@
+public class GameCharacter {
+    private Weapon weapon;
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void attack() {
+        if (weapon == null) {
+            System.out.println("No Weapon!");
+        } else {
+            weapon.attack();
+        }
+    }
+}
